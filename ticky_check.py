c35861eaa597a1bc530b3381a0863a7e343c5483
Ryan Davis <ryandavisim@gmail.com>
2024-06-03 14:04:34 -0400

    Updated the script to include the new user class format to help with data processing, now successfully reads in the file and creates the CSV

--- ticky_check.py
+++ ticky_check.py
@@ -1,9 +1,9 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
 import csv
 import os
 import re
 import operator
-import sys
+from User import User
 
 '''This program takes a log file and generates a small html report
 
@@ -11,15 +11,20 @@ This report contains the type and number of "ERRORS" in a log file, how many tim
 error occurred, sorted from greatest to least 
 '''
 
+'''This script can be made more efficient by creating a dict from the user and associating the name with
+a User object, this would reduce the amount of time it takes to search for a user to check if they exist in the
+user_list function. Right now it has to iterate over the list twice per function call, !!NO GOOD!!'''
+#TODO Refactor this user list to a user dict
 def read_in_log(file_path):
+    '''Reads in a Log File and creates two CSV files
+     One containing a list of users and their actions, the other containing errors'''
     error_lines = {}
-    user_stats_errors = {}
-    user_stats_info = {}
+    user_list = []
     with open(file_path, 'r') as file:
         for cur_line in file:
             if "ERROR" in cur_line:
                 # Captures the ERROR {error type} (user_name)
-                # and stores {error type} in regext register one
+                # and stores {error type} in regex register one
                 pattern = r"ERROR\s(.*)\s\((.*)\)"
                 current_error_line = re.search(pattern, cur_line)
 
@@ -30,32 +35,42 @@ def read_in_log(file_path):
                 else:
                     error_lines[current_error_line[1]] = error_lines[current_error_line[1]] + 1
                 #Updates the number of errors associated with the username
-                if current_error_line[2] not in user_stats_errors:
-                    user_stats_errors[current_error_line[2]] = 1
+                if current_error_line[2] not in user_list:
+                    #Create a new user if the username has not been seen before
+                    new_error_user = User(current_error_line[2], 0, 1)
+                    user_list.append(new_error_user)
                 else:
-                    user_stats_errors[current_error_line[2]] = user_stats_errors[current_error_line[2]] + 1
-            if "INFO" in cur_line:
-                pattern = r"INFO\s(.*)\s\((.*)\)"
-                current_info = re.search(pattern, cur_line)
-                if current_info[2] not in user_stats_info:
-                    user_stats_info[current_info[2]] = 1
-                else:
-                    user_stats_info[current_info[2]] = user_stats_info[current_info[2]] + 1
-        file.close()
-    sort_user(user_stats_errors)
-    sort_user(user_stats_info)
+                    user_list[user_list.index(current_error_line[2])].addErrorCount()
 
+            if "INFO" in cur_line:
+                #Matches onto the INFO {task information} (username)
+                #Captures username in the second regex register
+                pattern = r"INFO\s(.*)\s\((.*)\)"
+                current_info_line = re.search(pattern, cur_line)
+                if current_info_line[2] not in user_list:
+                    #Create a new user if the username has not been seen before
+                    new_info_user = User(current_info_line[2], 1, 0)
+                    user_list.append(new_info_user)
+                else:
+                    #Update the user info in the list
+                    user_list[user_list.index(current_info_line[2])].addInfoCount()
+
+        file.close()
     write_errors(error_lines)
-    write_users(user_stats_info,user_stats_errors)
+    write_users(user_list)
     return
 
 def write_errors(error_lines):
-    '''Writes A CSV of the errors by most common, assumes passed in dict is sorted'''
+    '''Writes A CSV of the errors from most to least common'''
+    #Header Row List Attributes
     keys = ["Error", "Count"]
     with open("error_message.csv", "w") as file:
         writer = csv.writer(file)
         writer.writerow(keys)
         item_list = list(error_lines.items())
+
+        #Lambda sorts the list by comparing the int stored in the second position in the item tuple
+        #which corresponds to the number of errors
         item_list.sort(key=lambda a: a[1], reverse=True)
         for tuple in item_list:
             row = [tuple[0], tuple[1]]
@@ -64,16 +79,23 @@ def write_errors(error_lines):
 
 
 
-def write_users(users_info, users_errors):
+def write_users(user_list):
+    '''Writes a list of users to the csv file, sorted alphabetically by username'''
+    #Header Row List Attributes
     keys = ["Username", "INFO", "ERROR"]
+    #This Lambda sorts the list alphabetically
+    user_list.sort(key=lambda a: a.getusername())
     with open("user_statistics.csv", "w") as file:
         writer = csv.writer(file)
         writer.writerow(keys)
-        user_names = list(users_info.keys())
-        user_names.sort()
-        for user_name in user_names:
-            row = [user_name, users_info[user_name], users_errors[user_name]]
-            writer.writerow(row)
+        user_count = 0
+        for user in user_list:
+            if user_count < 8:
+                row = [user.getusername(), user.get_info_number(), user.get_error_number()]
+                writer.writerow(row)
+                user_count += 1
+            else:
+                break
         file.close()
 
 #Two useful debug functions to see the log file printed in the expected manner
@@ -81,16 +103,12 @@ def sort_errors(error_lines):
     print(sorted(error_lines.items(), key=operator.itemgetter(1), reverse=True))
     return error_lines
 
-def sort_user(user_dict):
-    print(sorted(user_dict.items(), key=operator.itemgetter(0)))
-    return user_dict
 
 
 
 def main():
     log_file_path = os.path.join(os.getcwd() + "\syslog.log")
     print(log_file_path)
-    #log_file_path = sys.argv[1]
     read_in_log(log_file_path)
 
 if __name__ == "__main__":
